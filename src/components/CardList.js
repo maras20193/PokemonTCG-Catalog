9b3a33e6f651487845c5372df7484b6df802c6c5
Lukasz Markowski <lucas20193@gmail.com>
2020-05-15 09:50:06 +0200

    finish basic files structure

---
+++ src/components/CardList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = () => {
+  return (
+    <>
+      <div>
+        __________I am a CardList!!!__________ This is my Children:
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </>
+  );
+};
+
+export default CardList;
